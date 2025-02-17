c0a5d3c86773ef101a5fe2bd959c2a31688f709a
jimpinto <j_pinto@mail.fhsu.edu>
2025-02-16 23:22:32 -0600

    added matplotlib. converted closed costs to a numpy array. created graphs and showed them.

--- main.py
+++ main.py
@@ -18,9 +18,18 @@ for ticker in mytickers:
     last10days = []
     for date in hist['Close']:
         last10days.append(date)
-    myarray = np.array(last10days)
-    plt.plot(myarray)
-    plt.show()
+    if len(last10days) == 10:
+        myarray = np.array(last10days)
+        max_price = myarray.max() + (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.min()*.05)
+        plt.plot(myarray)
+        plt.xlabel('Data Points')
+        plt.ylabel('Closing Price')
+        plt.axis((9, 0, min_price, max_price))
+        plt.title(f"{ticker} last 10 closing price")
+        plt.show()
+    else:
+        print(f"Do not have 10 days of data. Only have {len(last10days)} days of data ")
 
 
 
