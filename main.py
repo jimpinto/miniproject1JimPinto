216ddec27168f398414c447b5d3332b3b24253f2
jimpinto <j_pinto@mail.fhsu.edu>
2025-02-16 22:53:44 -0600

    Fixed Dayhigh

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 ### INF601 - Advanced Programming in Python
 ### Jim Pinto
 ### Mini Project 1
+import pprint
+import yfinance as yf
+
+mytickers = ["MSFT", "AAPL", "GOOG", "NVDA", "AMC"]
+
+mydata = {}
+
+
+mytickers.sort()
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    mydata[ticker] ={'ticker': ticker,
+                     'dayHigh': result.info['dayHigh']
+                     }
+
+pprint.pprint(mydata)
+
+
+#msft = yf.Ticker("MSFT")
+
+# get all stock info
+#pprint.pprint(msft.info)
+
+# get historical market data
+#hist = msft.history(period="10d")
+
+#pprint.pprint(hist)
